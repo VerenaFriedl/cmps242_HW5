be0af69129076b977e8227b35479d153ed0b2212
vfriedl <vfriedl@eduroam-169-233-226-86.ucsc.edu>
2017-11-09 10:34:37 -0800

    python script to start with

---
+++ hw5.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+##########
+# File: hw5.py
+# Authors: Verena Friedl, Jon Akutagawa, Andrew Bailey
+# History: created       Nov 9, 2017
+#          last changed  Nov 9, 2017
+##########
+
+"""
+
+"""
+
+##########
+# Imports
+##########
+
+
+
+##########
+# Data
+##########
+
