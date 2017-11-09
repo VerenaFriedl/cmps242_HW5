4e3dbb0a125ea4a3eb423951244e7fd7e726ff0a
vfriedl <vfriedl@eduroam-169-233-226-86.ucsc.edu>
2017-11-09 11:24:52 -0800

    function for reading in tweet data

--- hw5.py
+++ hw5.py
@@ -15,9 +15,41 @@
 # Imports
 ##########
 
-
+import numpy as np
 
 ##########
 # Data
 ##########
 
+
+def readTweetData(filename):
+    """
+    Read in tweet collection of "HillaryClinton" and "realDonaldTrump". Other twitter handles would break this function.
+    :param filename: File to read tweet data from.
+    :return: list with handles and list with tweets; in order of file appearance.
+    """
+    fileH = open(filename, 'r')
+    header = fileH.readline().rstrip().split(',')
+    handles = []
+    tweets = []
+    goodHandles = ["HillaryClinton", "realDonaldTrump"]
+    for line in fileH.readlines():
+        splitline = line.rstrip('\n').split(',')
+
+        # line starts with twitter handle
+        if splitline[0] in goodHandles:
+            handles.append(splitline[0])
+            tweets.append(','.join(splitline[1:]))
+        # line is part of the last tweet
+        else:
+            tweets[len(tweets) - 1] = tweets[len(tweets) - 1] + "\n" + ','.join(splitline)
+
+    # test if data is read in correctly
+    # outfileH = open('./train_out.csv','w')
+    # outfileH.write(",".join(header) + "\n")
+    # for i in range(0, len(handles)):
+    #    outfileH.write(handles[i] + "," + tweets[i]+"\n")
+
+    return handles, tweets
+
+handles, tweets = readTweetData("./train.csv")
